e4a3d920ab6e0721d94a83c1461bc8fd01a66528
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-07-01 14:07:07 -0400

    Update war_7-1-20.py

--- war_7-1-20.py
+++ war_7-1-20.py
@@ -1,12 +1,26 @@
 def yes_no(arr):
+	new_list = arr[:]
+
+	while len(arr) > 2:
+		pops = []
+		for i in range(1, len(arr), 2):
+			new_list.append(arr[i])
+		for x in range(0, len(arr), 2):
+			pops.append(x)
+		pops.reverse()
+		for a in pops:
+			arr.pop(a)
 	output = []
-	for i in range(len(arr)):
-		print(arr, output)
-		if i % 2 == 0:
-			a = i % len(arr)
-			output.append(arr.pop(a))
-	return output
+	for i in range(0, len(new_list), 2):
+		output.append(new_list[i])
+
+	return output+[arr[0]]
+
+
 
 
 
 print(yes_no(['this', 'code', 'is', 'right', 'the']))
+
+lst = ['this', 'code', 'is', 'right', 'the'] + ['code', 'right'] + ['code']
+print(lst)
