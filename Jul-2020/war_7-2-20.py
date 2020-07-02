2dc6b5ddb36ae5f40da81fbc3075db0048e48ea7
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-07-02 13:14:43 -0400

    Update war_7-2-20.py

--- Jul-2020/war_7-2-20.py
+++ Jul-2020/war_7-2-20.py
@@ -3,3 +3,18 @@ def multiples(m, n):
 	for i in range(1, m+1):
 		output.append(n*i)
 	return output
+
+
+
+def likes(names):
+	if len(names) > 3:
+		return f'{names[0]}, {names[1]}, and {len(names)-2} others like this'
+	elif len(names) > 2:
+		return f'{names[0]}, {names[1]}, and {names[2]} like this'
+	elif len(names) > 1:
+		return f'{names[0]} and {names[1]} like this'
+	elif len(names) > 0:
+		return f'{names[0]} like this'
+	else:
+		return 'no one likes this'
+
