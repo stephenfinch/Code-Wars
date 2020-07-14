9f3ba3846ea801a4eba1663c618bf9915c3af4e7
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-07-14 00:13:00 -0400

    Update war_7-13-20.py

--- Jul-2020/war_7-13-20.py
+++ Jul-2020/war_7-13-20.py
@@ -1,13 +1,12 @@
 def is_divisible(n,x,y):
 	return n % x == 0 and n % y == 0
 
-
+import math
 def halving_sum(n):
 	den, num = 2, n
-	while round(n/den) > 1:
-		num += round(n/den)
+	while math.floor(n/den) >= 1:
+		num += math.floor(n/den)
 		den = den*2
-		print(den, num)
 	return num
 
 print(halving_sum(25),47)
