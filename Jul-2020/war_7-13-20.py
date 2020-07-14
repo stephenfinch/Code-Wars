c7ff73735842148cd50d76d1493fb9877a1f0674
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-07-13 23:55:52 -0400

    Update war_7-13-20.py

--- Jul-2020/war_7-13-20.py
+++ Jul-2020/war_7-13-20.py
@@ -1,12 +1,12 @@
 def is_divisible(n,x,y):
 	return n % x == 0 and n % y == 0
 
+
 def halving_sum(n):
-	num = n
-	while '.' in str(n/2):
-		print(n/2)
-		num += n/2
-		n = n/2
+	den, num = 2, n
+	while round(n/den) > 1:
+		num += round(n/den)
+		n = round(n/den)
 	return num
 
 print(halving_sum(25),47)
