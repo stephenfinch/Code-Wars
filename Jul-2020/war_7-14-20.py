41b2bb5cdb18ea13170aa6d85b8a6988975e4c66
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-07-15 00:11:04 -0400

    Update war_7-14-20.py

--- Jul-2020/war_7-14-20.py
+++ Jul-2020/war_7-14-20.py
@@ -2,3 +2,9 @@ def get_middle(s):
 	while len(s) > 2:
 		s = s[1:-1]
 	return s
+
+def high_and_low(numbers):
+	numbers = numbers.split(' ')
+	for item in numbers:
+		numbers.append(numbers.pop(numbers.index(item)))
+	return max(numbers), min(numbers)
