85188ca5f1d1c0cf555a57eb5a8d695a7acc6e76
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-07-21 23:35:03 -0400

    Update war_7-21-20.py

--- Jul-2020/war_7-21-20.py
+++ Jul-2020/war_7-21-20.py
@@ -1,18 +1,13 @@
 def solve(arr):
 	i = 0
-
 	while len(arr) > 1:
 		arr.sort()
-		arr.reverse()
-
-		arr[0] -= 1
 		arr[-1] -= 1
-
-		if arr[-1] <= 0:
-			arr = arr[:-1]
+		arr[0] -= 1
 		if arr[0] <= 0:
 			arr = arr[1:]
-		
+		if arr[-1] <= 0:
+			arr = arr[:-1]
 		i += 1
 	return i
 
