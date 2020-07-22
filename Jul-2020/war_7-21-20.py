772506f4b522f4fea4906f82da2376fca87d0181
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-07-21 23:56:13 -0400

    Update war_7-21-20.py

--- Jul-2020/war_7-21-20.py
+++ Jul-2020/war_7-21-20.py
@@ -1,23 +1,10 @@
-'''
-def solve(arr):
-	i = 0
-	while len(arr) > 1:
-		arr.sort()
-		arr[-1] -= 1
-		arr[0] -= 1
-		if arr[0] <= 0:
-			arr = arr[1:]
-		if arr[-1] <= 0:
-			arr = arr[:-1]
-		i += 1
-	return i
-'''
-import math
 def solve(arr):
 	arr.sort()
 	if sum(arr[:-1]) < arr[-1]:
-		return sum(arr[:-1]), '==============='
-	return math.floor(((arr[-1]+arr[0])+arr[1])/2)
+		return sum(arr[:-1])
+	elif arr[0] > arr[-1]-arr[1]:
+		return arr[-1]
+
 
 
 print(solve([1,1,1]), 1)
