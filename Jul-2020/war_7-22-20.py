1112b7476acfd778bb0541ec4797517b81492193
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-07-22 21:57:28 -0400

    Update war_7-22-20.py

--- Jul-2020/war_7-22-20.py
+++ Jul-2020/war_7-22-20.py
@@ -6,3 +6,18 @@ def check_exam(arr1,arr2):
 		elif arr2[i] != "":
 			total -= 1
 	return total if total >= 0 else 0
+
+
+letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w', 'x', 'y', 'z']
+def solve(s):
+	output = [0,0,0,0]
+	for char in s:
+		if char.isdigit():
+			output[2] += 1
+		elif char in letters:
+			output[1] += 1
+		elif char.lower() in letters:
+			output[0] += 1
+		else:
+			output[-1] += 1
+	return output
