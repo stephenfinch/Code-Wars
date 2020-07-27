adc84e39a57dbcc24c6f22588d8af0e6158dae3e
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-07-27 12:14:58 -0400

    Update war_7-27-20.py

--- Jul-2020/war_7-27-20.py
+++ Jul-2020/war_7-27-20.py
@@ -1,1 +1,4 @@
 rooms = {1:{'name': 0, 'description': 0, 'completed': 0}, 2:{'name': 0, 'description': 0, 'completed': 0}, 3:{'name': 0, 'description': 0, 'completed': 0}}
+
+def capitalize_word(word):
+	return word.title()
