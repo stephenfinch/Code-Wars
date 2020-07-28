711bc10c663c6435e2cf8b13cfa5724909b2f05c
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-07-28 00:06:41 -0400

    Create war_7-28-20.py

---
+++ Jul-2020/war_7-28-20.py
@@ -0,0 +1,16 @@
+def consecutive_sum(num):
+	output = []
+	for x in range(1,num+1):
+		temp = []
+		for i in range(x,num+1):
+			if sum(temp) == num and temp not in output:
+				output.append(temp)
+			elif sum(temp) < num:
+				temp.append(i)
+				print(temp)
+			else:
+				break
+	return len(output) + 1
+
+
+print(consecutive_sum(15), 4)
