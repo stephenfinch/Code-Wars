1c12a3ce6a47f2a4b9230dd3df71f5ec2877256d
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-07-27 21:55:41 -0400

    Update war_7-27-20.py

--- Jul-2020/war_7-27-20.py
+++ Jul-2020/war_7-27-20.py
@@ -2,3 +2,10 @@ rooms = {1:{'name': 0, 'description': 0, 'completed': 0}, 2:{'name': 0, 'descrip
 
 def capitalize_word(word):
 	return word.title()
+
+def ordered_count(s):
+	s.sort()
+	output = []
+	for char in set(s):
+		output.append((char,s.count(char)))
+	return output
