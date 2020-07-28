5a744b7faf72410c8cc2febcb632a5707bf9ea26
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-07-27 22:19:18 -0400

    Update war_7-27-20.py

--- Jul-2020/war_7-27-20.py
+++ Jul-2020/war_7-27-20.py
@@ -10,3 +10,31 @@ def ordered_count(s):
 			output.append((char, s.count(char)))
 		seen.append(char)
 	return output
+
+def remove_duplicate_ids(table):
+	keys = sorted(list(table.keys()))
+	keys.reverse()
+	seen = []
+	for k in keys:
+		temp1, temp2 = set(table.get(k)), []
+		for i in temp1:
+			if i not in seen:
+				temp2.append(i)
+			seen.append(i)
+		temp2.reverse()
+		table.update({k:temp2})
+	return table
+
+
+
+b = {
+    "1": ["C", "F", "G"],
+    "2": ["A", "B", "C"],
+    "3": ["A", "B", "D"],
+}
+res_b = {
+    "1": ["F", "G"],
+    "2": ["C"],
+    "3": ["A", "B", "D"]
+}
+print(remove_duplicate_ids(b), res_b)
