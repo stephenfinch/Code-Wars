0f479bec8f5d024ff3238292857bbe31a4d31817
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-08-01 15:09:23 -0400

    Update war_8-1-20.py

--- Aug-2020/war_8-1-20.py
+++ Aug-2020/war_8-1-20.py
@@ -1,2 +1,15 @@
 def solve(s):
-	pass
+	spaces = []
+	for i in range(len(s)):
+		if s[i] == ' ':
+			spaces.append(i)
+	s = s.replace(' ', '')
+	s = list(s)
+	s.reverse()
+	for a in spaces:
+		print(a)
+		s.insert(' ', a)
+	return ''.join(s)
+
+
+print(solve("i love codewars"),"s rawe docevoli")
