6b4568eb2704edd2b0c30a741d85a5b99888036a
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-08-03 00:33:32 -0400

    update

--- Aug-2020/war_8-1-20.py
+++ Aug-2020/war_8-1-20.py
@@ -23,6 +23,10 @@ def solve(s,a,b):
 print(solve("codewars",1,5),"cawedors")
 print(solve("codingIsFun",2,100),"conuFsIgnid")
 
-from itertools import product 
+from itertools import product
 def solve(arr):
-	return max(product(arr[0], arr[1]))
+	nums = []
+	arr product(arr[0], arr[1])
+	for a in arr:
+		nums.append(a[0]*a[1])
+	return max(nums)
---
+++ Aug-2020/war_8-3-20.py
@@ -0,0 +1,7 @@
+from itertools import product
+def solve(arr):
+	nums = []
+	arr product(arr[0], arr[1])
+	for a in arr:
+		nums.append(a[0]*a[1])
+	return max(nums)
