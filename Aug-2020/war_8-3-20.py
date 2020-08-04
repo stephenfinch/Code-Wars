8a46d1bb2b6a11fc242487c243e44fd32d54ae4f
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-08-03 22:31:43 -0400

    Update war_8-3-20.py

--- Aug-2020/war_8-3-20.py
+++ Aug-2020/war_8-3-20.py
@@ -8,9 +8,17 @@ def solve(arr):
 		a *= max_[i]
 		b *= min_[i]
 	return max(a, b), min_, max_, a, b
-
-
-print(solve([[14,2],[0,-16],[-12,-16]]),3584)
+#print(solve([[14,2],[0,-16],[-12,-16]]),3584)
 
 def repeat_str(r, s):
 	return s*r
+
+
+def all_non_consecutive(arr):
+	output = []
+	for i in range(1, len(arr)):
+		if arr[i] - arr[i-1] != 1:
+			output.append({'i':i, 'n':arr[i]})
+	return output
+
+print(all_non_consecutive([1,2,3,4,6,7,8,10]), [{'i': 4, 'n': 6}, {'i': 7, 'n': 10}])
