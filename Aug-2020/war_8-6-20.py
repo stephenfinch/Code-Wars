8378f925f54da584e54cfc773b620190f9ece6ea
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-08-06 11:09:26 -0400

    Update war_8-6-20.py

--- Aug-2020/war_8-6-20.py
+++ Aug-2020/war_8-6-20.py
@@ -1,2 +1,15 @@
 def parts_sums(ls):
-	return [sum(ls)] + [sum(ls)-ls.pop(0) for x in range(len(ls))]
+	total = sum(ls)
+	output = [total]
+	for i in range(len(ls)):
+		total -= ls[i]
+		output.append(total)
+	return output
+
+
+def parts_sums(ls):
+	output = [0]
+	for i in range(len(ls) - 1, 0, -1):
+		output.insert(0, ls[i] + output[0])
+	output.insert(0, ls[0] + output[0])
+	return output
