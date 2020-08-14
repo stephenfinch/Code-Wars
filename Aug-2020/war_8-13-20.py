59600f34c45168dc361599a5fe04810271c1ec76
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-08-13 23:46:24 -0400

    Update war_8-13-20.py

--- Aug-2020/war_8-13-20.py
+++ Aug-2020/war_8-13-20.py
@@ -1,1 +1,24 @@
 def over_the_road(address, n):
+	#if address % 2 == 0:
+
+	houses = {}
+	left = 1
+	right = n*2
+	for i in range(1, n+1):
+		houses.update({i:[left, right]})
+		left += 2
+		right -= 2
+	return houses
+
+
+
+'''
+
+ 1|   |12
+ 3|   |10
+ 5|   |8
+ 7|   |6
+ 9|   |4
+11|   |2
+
+'''
