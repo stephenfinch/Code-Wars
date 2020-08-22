0c16479018c027acc25295c4c6acb6c5074b3eb6
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-08-22 16:51:59 -0400

    Update war_8-22-20.py

--- Aug-2020/war_8-22-20.py
+++ Aug-2020/war_8-22-20.py
@@ -1,2 +1,25 @@
 def summation(num):
 	return sum(range(1,num+1))
+
+
+def solve(s):
+	if test(s):
+		return 'OK'
+	for i in range(len(s)):
+		temp = s[:i]+s[i+1:]
+		if test(temp):
+			return 'remove one'
+	else:
+		return 'no answer'
+
+
+def test(s):
+	output = True
+	for i in range(len(s)):
+		if s[i] != s[-1-i]:
+			output = False
+	return output
+
+
+
+print(solve("abba"),"OK")
