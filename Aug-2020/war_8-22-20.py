45a254aae1784d8aa42670db8d0df6b0fc17ed18
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-08-23 00:12:29 -0400

    Update war_8-22-20.py

--- Aug-2020/war_8-22-20.py
+++ Aug-2020/war_8-22-20.py
@@ -1,7 +1,6 @@
 def summation(num):
 	return sum(range(1,num+1))
 
-
 def solve(s):
 	if test(s):
 		return 'OK'
@@ -18,6 +17,34 @@ def test(s):
 			output = False
 	return output
 
+def solve(a):
+	'''
+	if the count of the number to your left is less than you switch
+	if that count is equal and that number is bigger than you switch
+	loop backwards through the arr
+	restart ever time you switch
+	'''
+	loop, end_loop = True, False
+	while loop:
+		temp = a.slice()
+		if end_loop: loop = False
+		for i in range(len(a)-1,1,-1):
+			if a.count(a[i-1]) < a.count(a[i]):
+				a = swap(a, i-1, i)
+				break
+			elif (a.count(a[i-1]) == a.count(a[i])) and a[i-1] > a[i]:
+				a = swap(a, i-1, i)
+				break
+			#a == temp: end_loop = True
+	return a
 
-def solve(arr):
-	pass
+def swap(a, h, k):
+	a[h], a[k] = a[k], a[h]
+	return a
+
+p = [1, 1, 1, 0, 0, 6, 8, 8, 6, 2, 3, 5, 9]
+#print(solve([1,2,3,0,5,0,1,6,8,8,6,9,1]),[1,1,1,0,0,6,6,8,8,2,3,5,9])
+#print(solve(p),[1,1,1,0,0,6,6,8,8,2,3,5,9])
+
+
+print([1,4] == [1,4])
