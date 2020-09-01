fd86a4a9dced2deea8041dddbfcdcdaeafc99e27
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-08-31 21:59:21 -0400

    Update war_8-30-20.py

--- Aug-2020/war_8-30-20.py
+++ Aug-2020/war_8-30-20.py
@@ -1,7 +1,21 @@
-set_ = 'abcdefghijklmnopqrstuvwxyz'
+letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 def missing_alphabets(s):
+	s = list(s)
 	s.sort()
-	output, repeat = [], 0
-	for char in set_:
-		if char not in s:
+	output = []
+	while s:
+		temp_letters = letters.copy()
+		for i in range(len(temp_letters)-1, -1, -1):
+			if temp_letters[i] in s:
+				s.pop(s.index(temp_letters[i]))
+				temp_letters.pop(i)
+			else:
+				output.append(temp_letters[i])
+	return ''.join(sorted(output))
+
+
+
+
+
+print(missing_alphabets("abbccddeeffgghhiijjkkllmmnnooppqqrrssttuuvvwwxxy"),"ayzz")
