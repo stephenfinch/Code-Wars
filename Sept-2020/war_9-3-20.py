8f08048d8e6828d68c054ddf7484e79362f6fe63
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-09-03 21:07:43 -0400

    Update war_9-3-20.py

--- Sept-2020/war_9-3-20.py
+++ Sept-2020/war_9-3-20.py
@@ -9,13 +9,12 @@ b = ['are spying on','are collecting data on','are profiting from','are putting 
 c = ['christain children','white men','donald trump','conservatives','gun owners','the frogs','teenage boys','straight married couples','unborn babeis','members of the right wing media']
 d = ['to establish a new world order','to turn them gay','to cover up 9/11','to destroy the nuclear family','to achieve eternal life','to spread a satanist agenda','to start world war III','to suppress the population','to control the media narrative','to take away our guns','to overthrow the US government']
 
-
 def political_stance():
 	a_pick = a[random.randint(0,len(a)-1)]
 	b_pick = b[random.randint(0,len(b)-1)]
 	c_pick = c[random.randint(0,len(c)-1)]
 	d_pick = d[random.randint(0,len(d)-1)]
 	stance = [a_pick,b_pick,c_pick,d_pick]
-	print(' '.join(stance)+'!!!')
+	print(' '.join(stance)+'!!')
 
 political_stance()
