3ce30b73cd08aac99fcf5cf1009326c39844803e
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-09-10 22:35:16 -0400

    update

--- Sept-2020/war_9-8-20.py
+++ Sept-2020/war_9-8-20.py
@@ -1,13 +1,9 @@
 def add(num1, num2):
 	num1, num2, output = list(str(num1)), list(str(num2)), []
 	while num1 and num2:
-		print(num1[-1], num2[-1])
 		output.insert(0, str(int(num1.pop(-1)) + int(num2.pop(-1))))
 	if num1 or num2:
 		output.insert(0, max(num1, num2)[0])
-
-	return ''.join(output)
-
-
+	return int(''.join(output))
 
 print(add(122,81), 1103)
