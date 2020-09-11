3e3719e0251e974f9015725cc8a3f133338f19df
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-09-10 22:52:27 -0400

    Update war_9-10-20.py

--- Sept-2020/war_9-10-20.py
+++ Sept-2020/war_9-10-20.py
@@ -1,2 +1,10 @@
 def add(num1, num2):
-	pass
+	num1, num2, output, index = list(str(num1)), list(str(num2)), [], 0
+	while num1 and num2:
+		output.insert(0, str(int(num1.pop(-1)) + int(num2.pop(-1))))
+	left_over = num1 + num2
+	output = left_over + output
+
+	return ''.join(output)
+
+print(add(1111580,34523))
