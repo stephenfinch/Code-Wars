4f573841e7fa1f53c7db32d3648fbb573dcdbb45
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-09-15 00:16:47 -0400

    Update war_9-14-20.py

--- Sept-2020/war_9-14-20.py
+++ Sept-2020/war_9-14-20.py
@@ -8,5 +8,43 @@ def dup(arr):
 		output.append(new_word)
 	return output
 
-print(dup(['aaabbtee', 'akleekbwq']), [])
-print(dup(["putteellinen","keenness"]), ['putelinen','kenes'])
+#print(dup(["putteellinen","keenness"]), ['putelinen','kenes'])
+
+
+
+import random
+
+def swap(a, b, arr):
+	temp_val = arr[a]
+	arr[a] = arr[b]
+	arr[b] = temp_val
+	return arr
+
+def sort_items(arr):
+	for i in range(len(arr)-1):
+		for i in range(1, len(arr)):
+			if arr[i] > arr[i-1]:
+				arr = swap(i, i-1, arr)
+		temp_list = [x.show() for x in arr]
+		print(temp_list)
+			
+
+class Item:
+	
+	def __init__(self, size):
+		self.size = size
+		self.char = {0: '|', 1: '||', 2: '|||', 3: '||||'}
+
+	def __gt__(self, other):
+		if self.size > other.size:
+			return True
+		return False
+
+	def show(self):
+		return self.char.get(self.size)
+
+
+
+lst = [Item(random.randint(0,3)) for x in range(20)]
+
+sort_items(lst)
