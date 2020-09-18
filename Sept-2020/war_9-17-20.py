594589eb5e26f7059c3e01c1f915b9e77a230a8b
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-09-17 21:57:19 -0400

    Update war_9-17-20.py

--- Sept-2020/war_9-17-20.py
+++ Sept-2020/war_9-17-20.py
@@ -1,3 +1,4 @@
+'''
 def num_of_open_lockers(n):
 	doors = [False for x in range(1, n+2)]
 	for i in range(1, n+1): # -1 = closed, 1 = open
@@ -6,5 +7,19 @@ def num_of_open_lockers(n):
 				doors[j] = not doors[j]
 				#print(j)
 	return doors.count(True)
+'''
+
+
+
+def num_of_open_lockers(n):
+	doors = []
+	for a in range(1,n + 1):
+		factors=[]
+		for i in range(1, a + 1):
+			if a % i == 0:
+				factors.append(i)
+		doors.append(len(factors) % 2 == 0)
+	return doors.count(True)
+
 
 print(num_of_open_lockers(100))
