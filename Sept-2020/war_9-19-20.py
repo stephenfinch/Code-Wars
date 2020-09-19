fd6665f6ce96cfaad4add0a611237d0469096b03
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-09-19 01:14:43 -0400

    Update war_9-19-20.py

--- Sept-2020/war_9-19-20.py
+++ Sept-2020/war_9-19-20.py
@@ -1,5 +1,19 @@
 def solve(s):
-	for char in s:
-		if char in 'qwertyuiopasdfghjklzxcvbnm':
-			s.replace(char, ' ')
-	return s
+	nums = []
+	while s:
+		temp = ''
+		while s[0].isnumeric():
+			temp += s[0]
+			if len(s) > 1:
+				s = s[1:]
+			else:
+				s = ['a']
+		if temp:
+			nums.append(int(temp))
+		s = s[1:]
+	return max(nums)
+
+
+
+
+print(solve('av223esa4vasd19f1'))
