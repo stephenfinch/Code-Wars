fe51be444d872fca3553cc57953e7391194a77a6
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-09-20 23:28:22 -0400

    Update war_9-20-20.py

--- Sept-2020/war_9-20-20.py
+++ Sept-2020/war_9-20-20.py
@@ -1,18 +1,16 @@
-
 def triangle(row):
+	count = 0
 	while len(row) > 1:
-		temp_row,  = ''
-		for i in range(0, len(row) - 2):
-			  temp_row += 'RGB'.replace(row[i]).replace(row[i+1])
+		temp_row = ''
+		for i in range(0, len(row) - 1):
+			if row[i] == row[i+1]:
+				temp_row += row[i]
+			else:
+				temp_row += 'RGB'.replace(row[i], '').replace(row[i+1], '')
 		row = temp_row
+		print(' '*(count//2) + row)
+		count += 1
 	return row
 
 
-
-'''
-Colour here:        G G        B G        R G        B R
-Becomes colour:      G          R          B          G
-'''
-
-
-print('ABC'.replace('B', '').replace('A', ''))
+print(triangle('RBRGBRBGGRRRBGBBBGG'), 'G')
