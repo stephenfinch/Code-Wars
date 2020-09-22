f03838763636f51c8e47ed578d0bfb2360233d87
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-09-21 20:35:42 -0400

    update

--- Sept-2020/war_9-20-20.py
+++ Sept-2020/war_9-20-20.py
@@ -1,5 +1,4 @@
 def triangle(row):
-	count = 0
 	while len(row) > 1:
 		temp_row = ''
 		for i in range(0, len(row) - 1):
@@ -8,8 +7,6 @@ def triangle(row):
 			else:
 				temp_row += 'RGB'.replace(row[i], '').replace(row[i+1], '')
 		row = temp_row
-		print(' '*(count//2) + row)
-		count += 1
 	return row
 
 
