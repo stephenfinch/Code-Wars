75a39252a2de67b8886f7d7e8d0bef2fc104ff38
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-09-22 22:42:03 -0400

    Update war_9-22-20.py

--- Sept-2020/war_9-22-20.py
+++ Sept-2020/war_9-22-20.py
@@ -1,2 +1,14 @@
 def arr(n=0):
 	return [x for x in range(n)]
+
+
+import random
+
+def squares(n):
+	return [x**2 for x in range(1, n+1)]
+
+def num_range(n, start, step):
+	return [x for x in range(start, start+(n*step), step)]
+
+def rand_range(n, mn, mx):
+	return [random.randint(mn, mx) for x in range(n)]
