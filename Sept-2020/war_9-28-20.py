2d91bf69d1443bc42eb1e2ea7f3c9c2003102eb1
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-09-28 23:01:49 -0400

    Update war_9-28-20.py

--- Sept-2020/war_9-28-20.py
+++ Sept-2020/war_9-28-20.py
@@ -1,10 +1,8 @@
 def pairs(a):
-	sets, count = [(a[i], a[i+1]) for i in range(0, len(a)-1, 2)], 0
-	for item in sets:
-		if item[0] <= item[1]:
-			count += 1
-	return count
+	b = [abs(a[i] - a[i+1]) for i in range(0, len(a)-1, 2)]
+	return b.count(True)
 	
 
 
 print(pairs([1,2,5,8,-4,-3,7,6,5]),3)
+print(pairs([21, 20, 22, 40, 39, -56, 30, -55, 95, 94]),2)
