126275af3c44eb251cca29a0fbb841afc4b6444e
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-10-09 16:57:49 -0400

    Update war_10-9-20.py

---
+++ Oct-2020/war_10-9-20.py
@@ -0,0 +1,17 @@
+def unique_in_order(arr):
+	output = [arr[0]]
+	for i in range(1,len(arr)):
+		if arr[i] != output[-1]:
+			output.append(arr[i])
+	return output
+
+
+def persistence(n):
+	output, count = 1, 0
+	while len(str(n)) > 1:
+		for x in str(n):
+			output *= int(x)
+		count += 1
+		n = output
+		print(count, n)
+	return count
