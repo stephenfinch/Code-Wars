1cb41558f9af0b129d79449d5519f398f6fab43c
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-10-11 22:54:11 -0400

    Update war_10-11-20.py

--- Oct-2020/war_10-11-20.py
+++ Oct-2020/war_10-11-20.py
@@ -7,3 +7,35 @@ def solve(s):
 			totals.append(count)
 			count = 0
 	return max(totals)
+
+
+import random
+
+class Card:
+	def __init__(self, level, subject, title, text):
+		# Shown Attributes
+		self.level = level
+		self.subject = subject
+		self.title = title
+		self.text = text
+
+		# Hidden Attributes
+		self.date_created = ''
+		self.card_id = 00000
+		self.total_study_time = 0
+
+	def delete_card(self):
+		pass
+
+	def edit_title(self, new_title):
+		self.title = new_title
+
+	def edit_text(self, new_text):
+		self.text = new_text
+
+	def study_game(self):
+		body_words = self.text.split(' ')
+		for i in range(len(body_words)):
+			if random.randint(0,3) < 1:
+				body_words.pop(i)
+		return body_words
