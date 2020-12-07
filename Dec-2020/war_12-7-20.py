98ecf56a704556cf03aa786c19cc011a30af9df5
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-12-07 17:53:35 -0500

    Update war_12-7-20.py

--- Dec-2020/war_12-7-20.py
+++ Dec-2020/war_12-7-20.py
@@ -1,8 +1,15 @@
 import math
 def sum_square_even_root_odd(nums):
-	# if even - square
-	# if odd - root
 	return round(sum([x**2 if x%2==0 else math.sqrt(x) for x in nums]), 2)
 
+#print(sum_square_even_root_odd([2,2,2,2,2,3,3]))
 
-print(sum_square_even_root_odd([2,2,2,2,2,3,3]))
+
+def check_password(s):
+	_len, upper, lower, digit, special = 7<len(s)<21, False, False, False, False
+	for i in set(s):
+		if True:
+			pass
+	return _len
+
+print(check_password('aaaaaaaaggggggggggggg'))
