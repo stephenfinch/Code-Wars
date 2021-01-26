a9412c51ebf683167cd30c9c53bde459177427dc
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2021-01-25 21:19:22 -0500

    Update war_1-25-21.py

--- Jan-2021/war_1-25-21.py
+++ Jan-2021/war_1-25-21.py
@@ -1,2 +1,14 @@
 def consecutive_sum(num):
-	pass
+	output = []
+	for x in range(1,num+1):
+		temp = []
+		for i in range(x,num+1):
+			if sum(temp) == num and temp not in output:
+				output.append(temp)
+			elif sum(temp) < num:
+				temp.append(i)
+			else:
+				break
+	return len(output) + 1
+
+print(consecutive_sum(100))
